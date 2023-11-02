c4f4b92f4b4410d5c130378682b78bdbebcacacb
Shakezidin <sinuzidin@gmail.com>
2023-11-02 19:53:06 +0530

    unit testing basic for study

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Shakezidin/unittesting/math" // Import the math package you've created
+)
+
 func main() {
-	
+	resultAdd := math.Add(1, 5)
+	resultSubtract := math.Subtract(7, 3)
+	resultDivision := math.Division(10, 2)
+	resultMultiplication := math.Multiplication(3, 4)
+
+
+	// Print the results
+	fmt.Printf("Addition Result: %d\n", resultAdd)
+	fmt.Printf("Subtraction Result: %d\n", resultSubtract)
+	fmt.Printf("Division Result: %f\n", resultDivision)
+	fmt.Printf("Multiplication Result: %d\n", resultMultiplication)
 }
